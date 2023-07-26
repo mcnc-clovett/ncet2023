f8f466a8c8d85476d760cb314378d13854c0da26
Chris Lovett <clovett@mcnc.org>
2023-07-26 08:50:57 -0400

    Initial commit

---
+++ Create-Scopes.ps1
@@ -0,0 +1,48 @@
+﻿# Set the global options for the server
+Set-DhcpServerv4OptionValue -DnsDomain 'ad.emsiensi.org' -DnsServer '192.168.100.108'
+
+# Get the scope data from the CSV file
+$dhcpData = Import-Csv C:\Users\Administrator\Downloads\DHCP_Mess.csv
+
+# Create the scopes
+<#
+foreach ( $d in $dhcpData ) {
+    Add-DhcpServerv4Scope -Name $d.name -StartRange $d.start -EndRange $d.end -State Active -SubnetMask $d.netmask -LeaseDuration $d.lease -Type Dhcp
+    
+    Set-DhcpServerv4OptionValue -ScopeId $d.scopeid -Router $d.gateway
+}
+#>
+<#
+foreach ( $d in $dhcpData ) {
+    Add-DhcpServerv4Scope `
+        -Name $d.name `
+        -StartRange $d.start `
+        -EndRange $d.end `
+        -State Active `
+        -SubnetMask $d.netmask `
+        -LeaseDuration $d.lease `
+        -Type Dhcp
+
+    Set-DhcpServerv4OptionValue `
+        -ScopeId $d.scopeid `
+        -Router $d.gateway
+}
+#>
+<#
+foreach ( $d in $dhcpData ) {
+    $scope = [pscustomobject]@{
+        Name = $d.Name
+        StartRange = $d.start
+        EndRange = $d.end
+        State = 'Active'
+        SubnetMask = $d.netmask
+        LeaseDuration = $d.lease
+        Type = 'Dhcp'
+    }
+    $scope | Add-DhcpServerv4Scope
+
+    Set-DhcpServerv4OptionValue `
+        -ScopeId $d.scopeid `
+        -Router $d.gateway
+}
+#>
---
+++ Create-Users.ps1
@@ -0,0 +1,45 @@
+﻿$newOu = New-ADOrganizationalUnit -Name "Dream Team" -Path "DC=ad,DC=emsiensi,DC=org" -ProtectedFromAccidentalDeletion $false -PassThru
+
+foreach ( $g in "Google","Zscaler" ) {
+    New-ADGroup -Name $g -Path $newOu.DistinguishedName -GroupCategory Security -GroupScope Global
+}
+
+$users = Import-Csv C:\Users\Administrator\Downloads\cne_users.csv
+
+foreach ( $u in $users ) {
+    $groups = $u.groups -split ','
+    
+    $givenName = $u.givenname
+    $surname = $u.surname
+    $name = "$($u.givenname) $($u.surname)"
+    $displayName = $name
+    $samAccountName = $u.givenname.Substring(0,1) + $u.surname
+    $userPrinName = $samAccountName + "@ad.emsiensi.org"
+    $emailAddress = $samAccountName + "@emsiensi.org"
+    $path = $newOu
+    $changePwdLogon = $true
+    $cannotChgPwd = $false
+    $pwdNeverExp = $false
+    $accountPwd = (ConvertTo-SecureString -AsPlainText -String 'B!gc00t!3' -Force)
+
+    New-ADUser `
+        -Name $name `
+        -GivenName $givenName `
+        -Surname $surname `
+        -DisplayName $displayName `
+        -SamAccountName $samAccountName `
+        -UserPrincipalName $userPrinName `
+        -EmailAddress $emailAddress `
+        -Path $newOu `
+        -ChangePasswordAtLogon $changePwdLogon `
+        -CannotChangePassword $cannotChgPwd `
+        -PasswordNeverExpires $pwdNeverExp `
+        -AccountPassword $accountPwd `
+        -Enabled $true
+
+    if ( $groups ) {
+        foreach ( $r in $groups ) {
+            Add-ADGroupMember -Identity $r -Members $samAccountName
+        }
+    }
+}
